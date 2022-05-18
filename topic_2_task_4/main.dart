33bf94f66c674b893c4c6b002aa5cf22d6f4287b
Wolfram-180 <sergei.ulvis@gmail.com>
2022-05-18 15:19:05 +0300

    p. 7

--- topic_2_task_4/main.dart
+++ topic_2_task_4/main.dart
@@ -19,4 +19,28 @@ void main() {
   print(dyn.runtimeType);
   dyn = text;
   print(dyn.runtimeType);
+  // result: dyn became String type
+  // reason: dynamic can change type in run-time
+
+  // 5
+  final fin = 1;
+  const con = 1;
+
+  //fin = 2;
+  // result: The final variable 'fin' can only be set once.
+  // reason: final can be assigned in run-time - but only once
+
+  //con = 2;
+  // result: Constant variables can't be assigned a value. Try removing the assignment, or remove the modifier 'const'
+  // reason: const to be assigned before run-time
+
+  // В чем отличия final от const?
+  // final - 1 раз в рантайме, const - установка значения до компиляции
+
+  // 6
+  a = 5; // var a - conflict with var a from p. 1. Создать глобальную переменную типа int с именем a
+  print(a % 2 == 0 ? 'четное' : 'нечетное');
+
+  // 7
+  print('I \u2665 dart');
 }
