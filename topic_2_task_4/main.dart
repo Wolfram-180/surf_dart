d9d45384d35438b4e85b03f76192c54738d9823c
Wolfram-180 <sergei.ulvis@gmail.com>
2022-05-18 16:18:27 +0300

    p. 9

--- topic_2_task_4/main.dart
+++ topic_2_task_4/main.dart
@@ -1,3 +1,5 @@
+import "dart:collection";
+
 // 1
 var a = 0;
 
@@ -43,4 +45,46 @@ void main() {
 
   // 7
   print('I \u2665 dart');
+
+  // 8
+  var list = <int>[1, 2, 3, 4, 5, 6, 7, 8]; // a
+
+  print(list.length); // b
+
+  list.sort((b, a) => a.compareTo(b));
+  print(list); // c
+
+  var newList = list.sublist(5, 8);
+  print(newList); // d
+
+  print(list.indexOf(5)); // e
+
+  list.sort((a, b) => a.compareTo(b));
+  print(list);
+  list.removeWhere((val) => val >= 5);
+  print(list); // f
+
+  final new_vals = [10, 20, 30];
+  list.replaceRange(0, 3, new_vals);
+  print(list); // g
+
+  // 9
+  Map<String, String> numberBook = {'Иван': '2264865'};
+  numberBook['Татьяна'] = '89523366684';
+  numberBook['Олег'] = '84952256575'; // a
+
+  numberBook.forEach((key, value) {
+    print(key + ' : ' + value);
+  }); // b
+
+  numberBook['Екатерина'] = '2359942'; // c
+
+  numberBook =
+      SplayTreeMap<String, String>.from(numberBook, (a, b) => a.compareTo(b));
+  print(numberBook);
+
+  final sortedBack =
+      SplayTreeMap<String, String>.from(numberBook, (b, a) => a.compareTo(b));
+
+  print(sortedBack); // d
 }
