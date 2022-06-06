9993b7614418991feb65b82618116241065ef102
Wolfram-180 <sergei.ulvis@gmail.com>
2022-06-06 11:13:09 +0300

    2_10 in proc

---
+++ topic_2_task_10/user.dart
@@ -0,0 +1,10 @@
+class User {
+  final String firstName;
+  final String lastName;
+  final DateTime birthDay;
+
+  User(
+      {required this.firstName,
+      required this.lastName,
+      required this.birthDay});
+}
