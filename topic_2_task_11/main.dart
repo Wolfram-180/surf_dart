5503233529769cd1a5245483dc9863bca0cfd075
Wolfram-180 <sergei.ulvis@gmail.com>
2022-06-12 17:49:21 +0300

    2-11

--- topic_2_task_11/main.dart
+++ topic_2_task_11/main.dart
@@ -1,6 +1,27 @@
 import 'dart:convert';
 import 'dart:io';
 
+Future<void> main() async {
+  // Future 1 - 3
+  var name = await getKbInput();
+  print(' Привет, $name');
+
+  name = await getKbInputHurry();
+  print(' Привет, $name');
+
+  var _dartstring = '';
+  getDartList().then((dartList) async {
+    for (var i = 0; i < dartList.length; i++) {
+      await Future.delayed(Duration(milliseconds: 150));
+      _dartstring += dartList[i];
+      print('$_dartstring');
+    }
+  });
+
+  // Stream 1 - 3
+}
+
+// Future 1 - 3
 Future<String?> getKbInput() async {
   print('Вводи своё имя, путник. Не торопись...');
   return await stdin.readLineSync(encoding: utf8);
@@ -8,7 +29,7 @@ Future<String?> getKbInput() async {
 
 Future<String?> getKbInputHurry({int sec = 5}) async {
   print(
-      'А теперь поторопись, путник! У тебя $sec секунд чтобы ввести свое имя... ');
+      'А теперь поторопись, путник! У тебя $sec секунд чтобы ЕЩЁ РАЗ :) ввести свое имя... ');
   var name = await stdin
       .transform(const Utf8Decoder())
       .transform(const LineSplitter())
@@ -25,9 +46,10 @@ Future<String?> getKbInputHurry({int sec = 5}) async {
   return name;
 }
 
-Future<void> main() async {
-  var name = await getKbInput();
-  print(' Привет, $name');
-  name = await getKbInputHurry();
-  print(' Привет, $name');
+Future<List> getDartList() async {
+  return Future.delayed(Duration(milliseconds: 500), () {
+    final String dart = 'DART! >>>>>>---==(________)------';
+    List<String> dartList = dart.split('');
+    return dartList;
+  });
 }
